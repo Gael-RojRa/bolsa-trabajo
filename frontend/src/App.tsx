88b011449f30681f1cc01efb0a1824182931da9f
Gerardo Gael Rojas Ramos <gerardo.rojasramos@ucr.ac.cr>
2025-06-04 10:12:42 -1200

    Añadir usuario de prueba en el seeder, mejorar la lógica de redirección en la página de inicio de sesión y optimizar la gestión de postulaciones en la página de ofertas.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,36 @@
-import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import LoginPage from './features/Auth/pages/LoginPage';
-import RegisterPage from './features/Auth/pages/RegisterPage';
-import Welcome from './features/Welcome/pages/Welcome';
-import JobIndex from './features/JobIndex/pages/JobIndex';
-import EmployerOffersPage from './features/Recruiter/pages/EmployerOffersPage';
-import OfferPostulationsPage from './features/Recruiter/pages/OfferPostulationsPage';
-import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/react';
-import { briefcaseOutline, personOutline } from 'ionicons/icons';
+import { Redirect, Route } from "react-router-dom";
+import { IonApp, IonRouterOutlet, setupIonicReact } from "@ionic/react";
+import { IonReactRouter } from "@ionic/react-router";
+import LoginPage from "./features/Auth/pages/LoginPage";
+import RegisterPage from "./features/Auth/pages/RegisterPage";
+import Welcome from "./features/Welcome/pages/Welcome";
+import JobIndex from "./features/JobIndex/pages/JobIndex";
+import EmployerOffersPage from "./features/Recruiter/pages/EmployerOffersPage";
+import OfferPostulationsPage from "./features/Recruiter/pages/OfferPostulationsPage";
+import {
+  IonTabs,
+  IonTabBar,
+  IonTabButton,
+  IonIcon,
+  IonLabel,
+} from "@ionic/react";
+import { briefcaseOutline, personOutline } from "ionicons/icons";
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import "@ionic/react/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
 
 /**
  * Ionic Dark Mode
@@ -35,10 +41,10 @@ import '@ionic/react/css/display.css';
 
 /* import '@ionic/react/css/palettes/dark.always.css'; */
 /* import '@ionic/react/css/palettes/dark.class.css'; */
-import '@ionic/react/css/palettes/dark.system.css';
+import "@ionic/react/css/palettes/dark.system.css";
 
 /* Theme variables */
-import './theme/variables.css';
+import "./theme/variables.css";
 
 setupIonicReact();
 
@@ -46,26 +52,18 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-      <Route exact path="/employer/offers" component={EmployerOffersPage} />
-      <Route exact path="/employer/offers/:id" component={OfferPostulationsPage} />
-        <Route exact path="/login">
-          <LoginPage />
-        </Route>
-        <Route exact path="/register">
-          <RegisterPage />
-        </Route>
-        <Route exact path="/">
-          <Welcome />
-        </Route>
-        <Route exact path="/jobs">
-          <JobIndex />
-        </Route>
-
-
-
+        <Route exact path="/employer/offers" component={EmployerOffersPage} />
+        <Route
+          exact
+          path="/employer/offers/:id"
+          component={OfferPostulationsPage}
+        />
+        <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/register" component={RegisterPage} />
+        <Route exact path="/" component={Welcome} />
+        <Route exact path="/jobs" component={JobIndex} />
       </IonRouterOutlet>
     </IonReactRouter>
-    
   </IonApp>
 );
 
